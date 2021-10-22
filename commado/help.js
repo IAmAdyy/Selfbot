456bbaecb540b2d96cc6a7f4d2778ba13d603d5d
adiixyz <82200440+Adiixyz@users.noreply.github.com>
2021-10-22 19:14:14 +0800

    Update help.js

--- commado/help.js
+++ commado/help.js
@@ -2,131 +2,135 @@ const help = (f, pushname, time, kentodd, sender) => {
 	return `_Hello *${pushname}* 👋_
 
 🌹 _Info Bot_
-🔖 _Base : https://github.com/adulalhy/SZ-SELF_
-🔖 _Recode : Adii_
 🔖 _Script : https://github.com/IAmAdyy/Selfbot_
 🔖 _Bot Name : AdyyBot_
 🔖 _Prefix : Multi Prefix_
 🔖 _Runtime : ${kentodd}_
+🔖 _Language : JavaScript_
+🔖 _Framework : NodeJS_
+🔖 _simple.js Enabled : false_
 
 🌹 _Other Information_
 🔖 _Username : ${pushname}_
-🔖 _Number : wa.me/${sender.split('@')[0]}_
+🔖 _Number : wa.me/${sender.split('@s.whatsapp.net')[0]}_
 🔖 _Time : ${time}_
 
 🌹 _Info_
-\`\`\`${f}sc
-${f}owner
-${f}ping
-${f}status
-${f}runtime
-${f}tes\`\`\`
+${f}sc_
+${f}owner_
+${f}ping_
+${f}status_
+${f}runtime_
+${f}tes_
 
 🌹 _Other Menu_
-\`\`\`${f}japanesemenu
-${f}russianmenu
-${f}menufvn
-${f}menuftoko\`\`\`
+${f}japanesemenu_
+${f}russianmenu_
 
 🌹 _Multi Session_
-\`\`\`${f}jadibot
-${f}scan
-${f}stopjadibot
-${f}byebot
-${f}listjadibot\`\`\`
+${f}jadibot_
+${f}scan_
+${f}stopjadibot_
+${f}byebot_
+${f}listjadibot_
 
 🌹 _Owner Bot_
-\`\`\`🔖 > [eval]
-🔖 => [eval async]
-🔖 $ [code]
-${f}term [code]
-${f}upswteks [teks]
-${f}upswvideo [send video]
-${f}upswimage [send image]
-${f}mode [button]
-${f}setprefix [buttton]
-${f}SETPREFIX2
-${f}antiviewonce [button]
-${f}setfakeimg [reply]
-${f}setthumb [reply]
-${f}bc [teks]
-${f}setimagereply
-${f}clearall [all chat]
-${f}setpp [reply picture]
-${f}setpp2full [error]
-${f}leave [group]
-${f}q [reply]
-${f}get [link]
-${f}sider [reply]
-${f}take [author|packname]
-${f}virtex [virus]
-${f}setnama [name]
-${f}sherelock [text|text]\`\`\`
+🔖 _> [eval]_
+🔖 _=> [eval async]_
+🔖 _$ [code]_
+${f}term [code]_
+${f}upswteks [teks]_
+${f}upswvideo [send video]_
+${f}upswimage [send image]_
+${f}mode [button]_
+${f}setprefix [buttton]_
+${f}SETPREFIX2_
+${f}bugloc_
+${f}antiviewonce [button]_
+${f}setfakeimg [reply]_
+${f}setthumb [reply]_
+${f}bc [teks]_
+${f}setimagereply_
+${f}clearall [all chat]_
+${f}setpp [reply picture]_
+${f}setpp2full [error]_
+${f}leave [group]_
+${f}q [reply]_
+${f}get [link]_
+${f}sider [reply]_
+${f}take [author|packname]_
+${f}virtex [virus]_
+${f}setnama [name]_
+${f}sherelock [text|text]_
 
 🌹 _Video/Vn_
-\`\`\`${f}tomp4 [reply sticker]
-${f}detikvideo [sec]
-${f}detikvn [sec]
-${f}tts [lang] [text]\`\`\`
+${f}tomp4 [reply sticker]_
+${f}detikvideo [sec]_
+${f}detikvn [sec]_
+${f}tts [lang] [text]_
 
 🌹 _Anime_
-\`\`\`${f}waifu
-${f}loli
-${f}husbu\`\`\`
+${f}waifu_
+${f}loli_
+${f}husbu_
 
 🌹 _Pictures_
-\`\`\`${f}cogan
-${f}cecan
-${f}darkjokes\`\`\`
+${f}cogan_
+${f}cecan_
+${f}darkjokes_
 
 🌹 _Group_
-\`\`\`${f}kick [reply]
-${f}add [reply]
-${f}group [button]
-${f}listadmin [admin group]
-${f}tagall [teks]
-${f}hidetag [teks]
-${f}rulesgroup [desc gc]
-${f}getpic [@tag]\`\`\`
+${f}kick [reply]_
+${f}add [reply]_
+${f}group [button]_
+${f}listadmin [admin group]_
+${f}tagall [teks]_
+${f}hidetag [teks]_
+${f}rulesgroup [desc gc]_
+${f}getpic [@tag]_
 
 🌹 _Other_
-\`\`\`${f}inspect [link grup]
-${f}readmore [text|text]
-${f}linkwa [query]
-${f}translate [lang] [text]\`\`\`
+${f}inspect [link grup]_
+${f}readmore [text|text]_
+${f}linkwa [query]_
+${f}translate [lang] [text]_
 
 🌹 _Sticker_
-\`\`\`${f}sticker [image]
-${f}toimg [sticker]\`\`\`
+${f}sticker [image]_
+${f}toimg [sticker]_
 
 🌹 _Downloader_
-\`\`\`${f}play [query]
-${f}video [query]
-${f}pinterest [query]
-${f}playstore [query]
-${f}igstory [username]
-${f}lirik [query]\`\`\`
+${f}play [query]_
+${f}video [query]_
+${f}pinterest [query]_
+${f}playstore [query]_
+${f}igstory [username]_
+${f}lirik [query]_
 
 🌹 _Sticker Command_
-\`\`\`${f}addcmd
-${f}delcmd
-${f}listcmd\`\`\`
+${f}addcmd_
+${f}delcmd_
+${f}listcmd_
 
 🌹 _Fun_
-\`\`\`${f}jadian
-${f}ganteng
-${f}beban
-${f}cantik
-${f}jelek
-${f}nganggur
-${f}hebat
-${f}wibu
-${f}pakboy
-${f}pakgirl
-${f}sange
-${f}nolep
-${f}jahat
-${f}baik\`\`\`
+${f}jadian_
+${f}ganteng_
+${f}beban_
+${f}cantik_
+${f}jelek_
+${f}nganggur_
+${f}hebat_
+${f}wibu_
+${f}pakboy_
+${f}pakgirl_
+${f}sange_
+${f}nolep_
+${f}jahat_
+${f}baik_
+
+🌹 _Next UPDATE!!!!_
+_Button Menu_
+_Simple Menu_
 
 _FITURNYA DIKIT AJA. JANGAN BULLY SAIA_
 `
